f5fb595d0e72ea2798f65bc850a69861f537ecbf
Ramit Roshan <110255669+RamitRoshan@users.noreply.github.com>
2024-03-20 22:54:43 +0530

    Added Solution - GfG to GitHub

---
+++ School/Print Elements of Array/print-elements-of-array.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            new Solution().printArray(arr, n);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+// class Solution {
+//     //Just print the space seperated array elements
+//     void printArray(int arr[], int n) {
+//         //   code here
+        
+        
+//         for(int i=0; i<arr.length; i++){
+            
+//             System.out.print(arr[i]);
+            
+//              if (i < n - 1) {
+//                 System.out.print(" "); // Print a space if it's not the last element
+//             }
+//         }
+        
+//     }
+// }
+
+
+
+class Solution {
+    // Just print the space-separated array elements
+    void printArray(int arr[], int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]);
+            if (i < n - 1) {
+                sb.append(" "); // Append a space if it's not the last element
+            }
+        }
+        System.out.print(sb.toString());
+    }
+}
