f562711148ad6c8814fc1221180395cf43abd4a4
Ramit Roshan <110255669+RamitRoshan@users.noreply.github.com>
2025-09-25 13:30:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sorted Array Search/sorted-array-search.java
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.java
@@ -1,48 +1,27 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class gfg {
-
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-            String inputLine[] = read.readLine().trim().split(" ");
-            int n = inputLine.length;
-            int arr[] = new int[n];
-
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-            int k = Integer.parseInt(read.readLine());
-
-            Solution obj = new Solution();
-
-            System.out.println(obj.searchInSorted(arr, k));
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     static boolean searchInSorted(int arr[], int k) {
         // Your code here
         
-        int n = arr.length;
+        if(arr.length == 0) return false;
         
-        boolean ans = false;
+        // int start = 0;
+        // int end = arr.length-1;
         
-        for(int i=0; i<n; i++){
+        // while(start <= end){
+            
+        //     int mid = start + (end-start)/2;
+            
+        //     if(arr)
+        // }
+        
+        for(int i=0; i<arr.length; i++){
             
             if(arr[i] == k){
                 return true;
             }
         }
+        return false;
         
-        return ans;
+        
     }
 }
