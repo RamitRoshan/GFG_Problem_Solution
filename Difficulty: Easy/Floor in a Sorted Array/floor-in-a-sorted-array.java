f6c8820c1bc80cfc1cdc98199c7faacc97de9987
Ramit Roshan <110255669+RamitRoshan@users.noreply.github.com>
2025-09-30 19:40:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.java
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.java
@@ -1,54 +1,45 @@
 // class Solution {
 //     public int findFloor(int[] arr, int x) {
-//         // code here
-        
 //         int start = 0;
-//         int end = arr.length-1;
-        
-//         int ans = -1;   // initially maan lo floor nahi mila
-        
-//         while(start <= end){
-            
-//             int mid = start + (end-start)/2;
-            
-            
-//             //exact match → floor
-//             if(x == arr[mid]){
-//                 return mid; 
-//             }
-            
-//             // possible floor
-//             if(x >= arr[mid]){
-//                 ans =  mid;
-//                 start = mid + 1;
-                
-//             }else{
-//                 end = mid -1;
+//         int end = arr.length - 1;
+//         int ans = -1;   // -1 if no floor exists
+
+//         while (start <= end) {
+//             int mid = start + (end - start) / 2;
+
+//             if (arr[mid] <= x) {
+//                 ans = mid;          // arr[mid] is a possible floor
+//                 start = mid + 1;   // try to find a larger index with value <= x
+//             } else {
+//                 end = mid - 1;     // value too big, go left
 //             }
 //         }
-//         // agar exact match nahi mila, to last stored floor index return kar
 //         return ans;
 //     }
 // }
 
-
-
 class Solution {
     public int findFloor(int[] arr, int x) {
+        
         int start = 0;
-        int end = arr.length - 1;
-        int ans = -1;   // -1 if no floor exists
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (arr[mid] <= x) {
-                ans = mid;          // arr[mid] is a possible floor
-                start = mid + 1;   // try to find a larger index with value <= x
-            } else {
-                end = mid - 1;     // value too big, go left
+        int end = arr.length -1;
+        
+        int ans = -1;
+        
+        while(start <= end){
+            
+            int mid = start + (end-start)/2;
+            
+            if(arr[mid] <= x){
+                ans  = mid;
+                start = mid + 1;
+                
+            }else{
+                end = mid - 1;
             }
         }
+        
         return ans;
+        
     }
-}
+}    
