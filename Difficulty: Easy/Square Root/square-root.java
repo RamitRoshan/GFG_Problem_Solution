398cf81f5cd295ea8eb8d59223e5aade069ce9e6
Ramit Roshan <110255669+RamitRoshan@users.noreply.github.com>
2025-10-04 16:06:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square Root/square-root.java
+++ Difficulty: Easy/Square Root/square-root.java
@@ -1,6 +1,20 @@
 class Solution {
     int floorSqrt(int n) {
         // code here
-        return (int)Math.sqrt(n);
+        // return (int)Math.sqrt(n);
+        
+        //using Linear Search
+        
+        int ans = 1; //(if nothing happens, then at least it'll give 1)
+       
+        for(int i=1; i<=n; i++){
+            
+            if(i * i <= n){
+                ans = i;
+            }else{
+                break;
+            }
+        }
+        return ans;
     }
 }
