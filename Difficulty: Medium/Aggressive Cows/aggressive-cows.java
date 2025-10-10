5b993cbe40355881e91bb3a44d6bb90da6bbebf1
Ramit Roshan <110255669+RamitRoshan@users.noreply.github.com>
2025-10-10 10:05:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -0,0 +1,31 @@
+class Solution {
+    
+    public static boolean canWePlace(int[] stalls, int dist, int cows) {
+        int n = stalls.length; //size of array
+        int cntCows = 1; //no. of cows placed
+        int last = stalls[0]; //position of last placed cow.
+        for (int i = 1; i < n; i++) {
+            if (stalls[i] - last >= dist) {
+                cntCows++; //place next cow.
+                last = stalls[i]; //update the last location.
+            }
+            if (cntCows >= cows) return true;
+        }
+        return false;
+    }
+    
+    public int aggressiveCows(int[] stalls, int k) {
+        // code here
+        int n = stalls.length;
+        //sort the stalls[]:
+        Arrays.sort(stalls);
+        
+        int limit = stalls[n - 1] - stalls[0];
+        for (int i = 1; i <= limit; i++) {
+            if (canWePlace(stalls, i, k) == false) {
+                return (i - 1);
+            }
+        }
+        return limit;
+    }
+}
