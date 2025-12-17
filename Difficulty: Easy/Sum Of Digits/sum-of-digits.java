28271f42aa01451e85216aecfb4f0357a649e18f
Ramit Roshan <110255669+RamitRoshan@users.noreply.github.com>
2025-12-18 01:27:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum Of Digits/sum-of-digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    static int sumOfDigits(int n) {
+        // code here
+        //base condition
+        if(n == 0){
+            return 0;
+        }
+        return (n%10) + sumOfDigits(n/10);
+        
+    }
+    
+ 
+}
