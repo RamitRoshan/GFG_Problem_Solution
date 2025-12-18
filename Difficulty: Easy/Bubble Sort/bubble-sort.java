e19a4d6743277dde625ac1e741c3a108b8e4fd29
Ramit Roshan <110255669+RamitRoshan@users.noreply.github.com>
2025-12-19 02:43:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.java
+++ Difficulty: Easy/Bubble Sort/bubble-sort.java
@@ -1,60 +1,25 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
-// User function Template for Java
-
 class Solution {
-    // Function to sort the array using bubble sort algorithm.
-    public static void bubbleSort(int arr[]) {
-        // code here\\
-        int n = arr.length;
+    public void bubbleSort(int[] arr) {
+        // code here
+        bubble(arr, arr.length-1, 0);
+    }
+    
+    public void bubble(int[] arr, int row, int col){
+        if(row == 0){
+            return;
+        }
         
-        for(int i=0; i<n-1; i++){
-            for(int j=0; j<n-i-1; j++){
+        if(col < row){
+            if(arr[col] > arr[col+1]){
                 
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
+                //swap
+                int temp = arr[col];
+                arr[col] = arr[col+1];
+                arr[col+1] = temp;
             }
+            bubble(arr, row, col+1);
+        }else{
+            bubble(arr, row-1, 0);
         }
     }
 }
-
-//{ Driver Code Starts.
-
-class Sorting {
-    // method to print the Elements of the array
-    static void printArray(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i) System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
-        while (t > 0) {
-            String inputLine[] = br.readLine().trim().split(" ");
-            int n = inputLine.length;
-            int arr[] = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-            // calling bubbleSort() method
-            new Solution().bubbleSort(arr);
-
-            // calling printArray() method
-            printArray(arr);
-
-            t--;
-        }
-    }
-}
-// } Driver Code Ends
