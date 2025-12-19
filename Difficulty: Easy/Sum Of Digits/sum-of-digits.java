b7bf54394512df7082c48b28751b57163295aa1e
Ramit Roshan <110255669+RamitRoshan@users.noreply.github.com>
2025-12-19 22:40:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sum Of Digits/sum-of-digits.java
+++ Difficulty: Easy/Sum Of Digits/sum-of-digits.java
@@ -11,3 +11,21 @@ class Solution {
     
  
 }
+
+/*
+sumOfDigits(687)
+= 7 + sumOfDigits(68)
+= 7 + (8 + sumOfDigits(6))
+= 7 + 8 + (6 + sumOfDigits(0))
+= 7 + 8 + 6 + 0
+= 21
+Each recursive call:
+
+Takes last digit → n % 10
+
+Removes last digit → n / 10
+
+Stops when n == 0
+
+
+*/
